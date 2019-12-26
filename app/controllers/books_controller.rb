0eba87580292a84f3b9b5fe7c6b6d55083011d55
Monluxfer <shibarome@gmail.com>
2019-12-27 02:51:22 +0300

    header front

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,18 @@
 class BooksController < ApplicationController
+
   def home
   end
 
+  # контакты и прочее
   def help
   end
+
+  # корзина
+  def bag
+  end
+
+  # регистрация
+  def reg
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'books#home'
-  get 'books/help'
+  root 'books#home', as: 'home'
+  get 'books/help', as: 'help'
+  get 'books/bag', as: 'bag'
+  get 'books/reg', as: 'reg'
+  get 'books/god', as: 'god'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
